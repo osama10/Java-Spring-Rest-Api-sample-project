409b53396d8cd2a9d075658d589e212c73330d8b
Osama Bin Bashir <osama.bashir@hellofresh.com>
2022-03-28 23:25:16 +0200

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Osama");
+	}
+
+	@GetMapping
+	public List<String> firstAPI() {
+		return List.of("Allah hu Akbar");
+	}
+}
