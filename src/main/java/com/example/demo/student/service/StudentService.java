a4885528b4132b960473be8e3251910396221ecf
Osama Bin Bashir <osama.bashir@hellofresh.com>
2022-03-29 01:29:37 +0200

    create Student repo and inject it in StudentService

--- src/main/java/com/example/demo/student/service/StudentService.java
+++ src/main/java/com/example/demo/student/service/StudentService.java
@@ -1,6 +1,8 @@
 package com.example.demo.student.service;
 
 import com.example.demo.student.model.Student;
+import com.example.demo.student.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,21 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
+        return studentRepository.findAll();
+    }
+
+    /*
+    List.of(
                 new Student(1L, "Osama",  "osama@yahoo.com", LocalDate.of(1995, 12, 10), 25),
                 new Student(2L, "Shazeen",  "shzeen@yahoo.com", LocalDate.of(1996, 06, 13), 24)
         );
-    }
+     */
 }
