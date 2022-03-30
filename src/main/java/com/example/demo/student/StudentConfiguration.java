def3d1937b1ec634610c07fe16244201c253f0cb
Osama Bin Bashir <osama.bashir@hellofresh.com>
2022-03-30 19:46:14 +0200

    create student configuration

---
+++ src/main/java/com/example/demo/student/StudentConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import com.example.demo.student.model.Student;
+import com.example.demo.student.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfiguration {
+
+    @Bean
+    CommandLineRunner commandLineRunner (StudentRepository studentRepository) {
+        return args -> {
+            Student osama = new Student( "Osama",  "osama@yahoo.com", LocalDate.of(1995, 12, 10) ,25);
+            Student shazeen = new Student( "Shazeen",  "shzeen@yahoo.com", LocalDate.of(1996, 06, 13), 24);
+            studentRepository.saveAll(List.of(osama, shazeen));
+        };
+    }
+}
