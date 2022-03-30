729a1cc5b19477bfa485859e67e77e650d513d8f
Osama Bin Bashir <osama.bashir@hellofresh.com>
2022-03-30 23:40:55 +0200

    create put student api

--- src/main/java/com/example/demo/student/controller/StudentController.java
+++ src/main/java/com/example/demo/student/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path="api/v1/student")
@@ -29,5 +30,12 @@ public class StudentController {
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long id) {
         studentService.deleteStudent(id);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public  void updateStudent(@PathVariable("studentId") Long id,
+                               @RequestParam(required = false) Optional<String> name,
+                               @RequestParam(required = false) Optional<String> email) {
+        studentService.updateStudent(id, name, email);
     }
 }
--- src/main/java/com/example/demo/student/service/StudentService.java
+++ src/main/java/com/example/demo/student/service/StudentService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.sql.Struct;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -35,12 +37,42 @@ public class StudentService {
     }
 
     public void deleteStudent(Long id) {
-        Boolean userExist = studentRepository.existsById(id);
+        Boolean studentExist = studentRepository.existsById(id);
 
-        if (userExist) {
-            throw new IllegalArgumentException("User with id " + id + "doesn't exist");
+        if (!studentExist) {
+            throw new IllegalArgumentException("User with id " + id + " doesn't exist");
         }
 
         studentRepository.deleteById(id);
+
     }
+
+    @Transactional
+    public void updateStudent(Long id, Optional<String> name, Optional<String> email) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("Student of Id" + id + " doesn't exist")
+                );
+
+        if (name.isPresent()
+                && !name.get().isEmpty()
+                && !student.getName().equals(name.get())) {
+            student.setName(name.get());
+        }
+
+        if (email.isPresent()
+                && !email.get().isEmpty()
+                && !email.get().equals(student.getEmail())) {
+            Boolean studentExist = studentRepository.existsById(id);
+
+            if (!studentExist) {
+                throw new IllegalArgumentException("User with id " + id + " doesn't exist");
+            }
+
+            student.setEmail(email.get());
+        }
+
+    }
+
+
 }
