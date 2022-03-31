9ed74fe80c568fdd609adab663d8dc09d8ef95ec
Osama Bin Bashir <osama.bashir@hellofresh.com>
2022-03-31 02:20:12 +0200

    complete testing for StudentRepository

--- src/test/java/com/example/demo/student/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/repository/StudentRepositoryTest.java
@@ -1,31 +1,58 @@
 package com.example.demo.student.repository;
 
 import com.example.demo.student.model.Student;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository sut;
 
+    @AfterEach
+    void tearDown() {
+        sut.deleteAll();
+    }
+    
     @Test
-    void itShouldFindStudentByEmail() {
+    void test_whenCorrectEmailIsPassedToRepo_thenItShowsStudentExist() {
         // given
         String email = "osama@yahoo.com";
         Student student = new Student("Osama",
                 "osama@yahoo.com",
                 LocalDate.of(1995, 12, 19));
 
+        sut.save(student);
+
         // when
         Optional<Student> optionalStudent = sut.findStudentByEmail(email);
 
         // then
         assertTrue(optionalStudent.isPresent());
+    }
+
+    @Test
+    void test_whenCorrectEmailIsPassedToRepo_thenItShowsStudentDoesntExist() {
+        // given
+        String email = "osama1@yahoo.com";
+        Student student = new Student("Osama",
+                "osama@yahoo.com",
+                LocalDate.of(1995, 12, 19));
+
+        sut.save(student);
+
+        // when
+        Optional<Student> optionalStudent = sut.findStudentByEmail(email);
+
+        // then
+        assertFalse(optionalStudent.isPresent());
     }
 }
