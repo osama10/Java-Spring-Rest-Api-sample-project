29b94027af21da1cec039ed802d1c4ef649b9223
Osama Bin Bashir <osama.bashir@hellofresh.com>
2022-04-12 15:37:10 +0200

    enabled tests

--- src/test/java/com/example/demo/student/service/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/service/StudentServiceTest.java
@@ -138,7 +138,6 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
     void test_whenUpdateIsCalledEmailThatDoesntExist_studentServiceThrowsError() {
         Student student = new Student("Osama",
                 "osama@yahoo.com",
@@ -150,7 +149,7 @@ class StudentServiceTest {
                 .willReturn(Optional.of(student));
 
         given(studentRepository.existsById(studentId))
-                .willReturn(false);
+                .willReturn(true);
 
         assertThatThrownBy(() -> sut.updateStudent(studentId, Optional.empty(), Optional.of(newEmail)))
                 .isInstanceOf(IllegalArgumentException.class)
